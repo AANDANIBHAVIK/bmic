21b731a45c8d02924891f7d3759e8f39fbfe3325
admin <bhavikaandani012@gmail.com>
2022-12-02 13:46:17 +0530

    2/12/2022

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:bmic/splaceScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main()
+{
+  runApp(
+      GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      )
+  );
+}
